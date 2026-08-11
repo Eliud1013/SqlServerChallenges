72b4b016a842539b43ea4ea846562cc4143d2e34
eliud1013 <eliud1013@Someone>
2026-08-11 16:50:38 -0700

    feat: wire challenge detail page to GetChallengeInfoQuery
    Checkpoint: failure branch incomplete. Also drop unused Bogus references.

--- src/SqlServerChallenges.Web/Pages/Challenges/Detail.cshtml.cs
+++ src/SqlServerChallenges.Web/Pages/Challenges/Detail.cshtml.cs
@@ -1,9 +1,8 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SqlServerChallenges.Core.Data.Entities.Challenges;
 using SqlServerChallenges.Core.Features.Challenges.GetChallengeDetails.GetChallengeInfo;
 using SqlServerChallenges.Core.Features.Challenges.GetChallengeDetails.GetChallengeState;
-using SqlServerChallenges.Core.Services.QueryExecutor;
 
 namespace SqlServerChallenges.Web.Pages.Challenges;
 
@@ -11,22 +10,22 @@ public class Detail : PageModel
 {
     public ChallengeInfo Info { get; private set; } = null!;
     public ChallengeState State { get; private set; } = null!;
-    public string Slug { get; private set; } = null!;
-
-    public IActionResult OnGet(string slug)
+    private readonly ISender _sender;
+    public Detail(ISender sender)
     {
-        var rows = new List<string>();
-        var cols = new List<Dictionary<string, object?>>();
-        
-        Slug = slug;
-        Info = new ChallengeInfo(
-            "SELECT All",
-            "Write a query to return all columns from the Person.Person table.",
-            ChallengeDifficulty.Easy,
-            "Basic Queries",
-            new OutputTable(rows,cols));
+        _sender = sender;
+    }
+    
+    public async Task<IActionResult> OnGet(string slug)
+    {
+        var infoResult = await _sender.Send(new GetChallengeInfoQuery(slug));
 
-        State = new ChallengeState(CommentCount: 3, IsSolved: false);
+        if (infoResult.Failed)
+        {
+            //IDK
+        }
+        
+        Info = infoResult.Value;
 
         return Page();
     }
